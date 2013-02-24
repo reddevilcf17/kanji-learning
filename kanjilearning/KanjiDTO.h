42b0b28930c4d55e5dbaaba1e7bc4aa8a09e59ef
Red <Hung@Reds-MacBook-Pro.local>
2013-02-24 17:41:56 +0700

    added 2 file:

---
+++ kanjilearning/KanjiDTO.h
@@ -0,0 +1,13 @@
+//
+//  KanjiDTO.h
+//  kanjilearning
+//
+//  Created by Red on 2/24/13.
+//  Copyright (c) 2013 reddevil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KanjiDTO : NSObject
+
+@end

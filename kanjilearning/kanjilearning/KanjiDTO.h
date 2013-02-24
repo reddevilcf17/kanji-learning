0e49ddc70de6667aa1f696f05aa5898a738152b6
Red <Hung@Reds-MacBook-Pro.local>
2013-02-24 18:06:22 +0700

    đã đọc được

--- kanjilearning/kanjilearning/KanjiDTO.h
+++ kanjilearning/kanjilearning/KanjiDTO.h
@@ -9,5 +9,6 @@
 #import <Foundation/Foundation.h>
 
 @interface KanjiDTO : NSObject
+@property (weak) IBOutlet NSTextField *VietnameseMeaning;
 
 @end
--- kanjilearning/kanjilearning/KanjiDTO.m
+++ kanjilearning/kanjilearning/KanjiDTO.m
@@ -9,5 +9,23 @@
 #import "KanjiDTO.h"
 
 @implementation KanjiDTO
+- (IBAction)btGenerate:(id)sender {
+//        NSString *newContents = @"abcxyz"; // New string.
+//        NSError *error = nil;
+//        [newContents writeToFile:@"someFile.txt" atomically:YES encoding:NSUTF8StringEncoding error:&error];
+//        if (error) {
+//            // Some error has occurred. Handle it.
+//        }
+    
+    //Read file
+    NSString *contents = [NSString stringWithContentsOfFile:@"someFile.txt" encoding:NSUTF8StringEncoding error:NULL];
+    
+    //NSInteger integer = [contents integerValue];
+    
+    NSArray *lines = [contents componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+    NSInteger count = [lines count];
+    int r = rand() % count;
+    [_VietnameseMeaning setStringValue:[lines objectAtIndex:r]];
+}
 
 @end

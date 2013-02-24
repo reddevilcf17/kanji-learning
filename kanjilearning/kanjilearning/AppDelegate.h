5bc70a3f901dc1dfb203c40888eeb7722303fdb3
Red <Hung@Reds-MacBook-Pro.local>
2013-02-24 17:31:17 +0700

    this is the first

---
+++ kanjilearning/kanjilearning/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  kanjilearning
+//
+//  Created by Red on 2/24/13.
+//  Copyright (c) 2013 reddevil. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ kanjilearning/kanjilearning/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  kanjilearning
+//
+//  Created by Red on 2/24/13.
+//  Copyright (c) 2013 reddevil. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
